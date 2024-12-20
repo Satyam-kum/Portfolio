f60ada682cc3e55793c9eea26faa28c170e90a9a
Satyam Kumar Sharma <97312085+Satyam-kum@users.noreply.github.com>
2024-12-20 11:45:07 +0000

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
             >
-              Full Stack Developer
+              Blockchain Developer
             </motion.span>
             
             <motion.p
